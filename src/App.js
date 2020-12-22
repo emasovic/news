d5a81fa6911b379c3459ded0090c37565dcdf3d3
eko <elvismasovic@spinviewglobal.com>
2020-12-22 11:11:47 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Router, Route, Switch} from 'react-router-dom';
+
+import {CATEGORIES, CATEGORY, HOME, SEARCH, SINGLE_ARTICLE} from 'lib/routes';
+import history from 'lib/history';
+
+import Article from 'views/news/Article';
+import Categories from 'views/categories/Categories';
+import Search from 'views/search/Search';
+import Home from 'views/home/Home';
+import Category from 'views/categories/Category';
+
+import Nav from 'components/nav/Nav';
+
+import './App.scss';
+
+const CLASS = 'na-App';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router history={history}>
+			<Nav />
+			<div className={CLASS}>
+				<Switch>
+					<Route exact path={HOME} component={Home} />
+					<Route path={SINGLE_ARTICLE} component={Article} />
+					<Route path={CATEGORY} component={Category} />
+					<Route path={CATEGORIES} component={Categories} />
+					<Route path={SEARCH} component={Search} />
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {Provider} from 'react-redux';
+
+import store from './redux/store';
+import {initialize} from 'redux/application';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import './index.scss';
+
+store.dispatch(initialize());
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
