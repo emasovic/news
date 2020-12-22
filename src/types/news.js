a3db8e11818ceb5004307783cbd2add95c97bfe2
eko <elvismasovic@spinviewglobal.com>
2020-12-22 11:03:12 +0100

    added types

---
+++ src/types/news.js
@@ -0,0 +1,23 @@
+export const COUNTRY = {
+	gb: 'gb',
+	us: 'us',
+};
+
+export const COUNTRY_LABELS = {
+	[COUNTRY.gb]: 'GB',
+	[COUNTRY.us]: 'US',
+};
+
+export const DEFAULT_CRITERIA = {
+	country: COUNTRY.gb,
+};
+
+export const CATEGORIES = {
+	business: 'business',
+	entertainment: 'entertainment',
+	general: 'general',
+	health: 'health',
+	science: 'science',
+	sports: 'sports',
+	technology: 'technology',
+};
